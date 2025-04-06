6fec06a10c3e6df23d2f1cb5347e20231d19e0c3
priyanshpatel18 <priyansh.180804@gmail.com>
2025-04-06 16:47:55 +0530

    shifted the feeding database logic here

---
+++ src/lib/feedData.ts
@@ -0,0 +1,54 @@
+import prisma from "../db/prisma";
+import { TRANSFER } from "../types/params";
+import { getDatabaseClient } from "../utils/dbUtils";
+import { ensureTransferTableExists, insertTransferData } from "../utils/tableUtils";
+
+interface FeedData {
+  databaseId: string;
+  transactions: any;
+}
+
+export default async function feedData(webhookData: FeedData) {
+  console.time("Total feedData");
+  const { databaseId, transactions } = webhookData;
+
+  if (!databaseId || !transactions) return;
+
+  const database = await prisma.database.findUnique({
+    where: { id: databaseId },
+  });
+  if (!database) {
+    console.error(`Database with ID ${databaseId} not found.`);
+    return;
+  }
+
+  const db = await getDatabaseClient(database);
+
+  let transferTableChecked = false;
+  for (const txn of transactions) {
+    const type = txn.type;
+    switch (type) {
+      case TRANSFER:
+        if (!transferTableChecked) {
+          await ensureTransferTableExists(db, TRANSFER);
+          transferTableChecked = true;
+        }
+
+        const data = {
+          slot: txn.slot,
+          signature: txn.signature,
+          feePayer: txn.feePayer || txn.transaction?.message?.accountKeys[0] || "unknown",
+          fee: txn.fee || 0,
+          description: txn.description || null,
+          accountData: txn.accountData || [],
+          instructions: txn.instructions || [],
+        };
+
+        await insertTransferData(db, TRANSFER, data);
+        break;
+      default:
+        break;
+    }
+  }
+
+}
